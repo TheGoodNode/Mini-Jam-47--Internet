29762e4c634424860cefbc91e716a7789feea5e8
William <liams_1998@hotmail.com>
2020-02-08 03:32:49 +0200

    added player controls + sprites  + testing sprites

---
+++ Inside-Job/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    public float PlayerSpeed;
+
+    public GameObject requestHolder;
+
+    bool isHoldingRequest = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.MovePlayer();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HoldRequest();
+        }
+    }
+
+    private void HoldRequest()
+    {
+        if (isHoldingRequest)
+        {
+            Debug.Log("Started holding the request");
+            isHoldingRequest = true;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 position = rigidbody2d.position;
+
+        position +=  move * PlayerSpeed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+    }
+  
+
+}
