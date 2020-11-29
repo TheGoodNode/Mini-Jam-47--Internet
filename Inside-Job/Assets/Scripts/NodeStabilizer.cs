8e4e44a86e5ed90bd0c64d1e3c2a7213346a1e13
William AbouSharaf <liams_1998@hotmail.com>
2020-11-29 21:55:14 +0200

    Create node stabilizer script

---
+++ Inside-Job/Assets/Scripts/NodeStabilizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeStabilizer : MonoBehaviour
+{
+    public List<GameObject> nodeToSetActive;
+    public List<GameObject> nodeToSetInActive;
+
+    private void Awake()
+    {
+        nodeToSetActive.ForEach(gameObject =>{
+            gameObject.SetActive(true);
+        });
+
+        nodeToSetActive.ForEach(gameObject =>{
+            gameObject.SetActive(false);
+        });
+
+        //Destroy self since I need this gameobject on awake only
+        Destroy(gameObject);
+    }
+}
