02dc11bd0160cc05aa8d8744d04d6c9020d9ce8e
William AbouSharaf <liams_1998@hotmail.com>
2020-11-29 20:05:52 +0200

    Split player controls & player logic

---
+++ Inside-Job/Assets/Scripts/GameObject/Player/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    [HideInInspector] public bool playerIsOverEntryPoint = false;
+    [HideInInspector] public bool playerIsHoldingMessage = false;
+    [HideInInspector] public bool playerIsOverRequestEntryPoint = false;
+
+    void Update()
+    {
+        if (playerIsOverEntryPoint)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("OVER HERE!!!" + playerIsHoldingMessage);
+                if (!playerIsHoldingMessage)
+                {
+                    Debug.Log("Getting Message");
+                    // selectedEntry.GiveMessageToPlayer(requestHolder);
+                }
+                else if (playerIsHoldingMessage && !playerIsOverRequestEntryPoint)
+                {
+                    Debug.Log("Setting Message");
+                    // Transform message = requestHolder.transform.GetChild(0);
+                    // selectedEntry.SetMessageToSlot(message);
+                }
+            }
+        }
+    }
+
+    EntryPoint selectedEntry;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("EntryPoint"))
+        {
+            playerIsOverEntryPoint = true;
+            EntryPoint entryPoint = other.gameObject.GetComponent<EntryPoint>();
+            selectedEntry = entryPoint;
+            if (other.gameObject.tag == "RequestEntryPoint")
+            {
+                Debug.LogWarning("over request point");
+                playerIsOverRequestEntryPoint = true;
+            }
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("EntryPoint"))
+        {
+            playerIsOverEntryPoint = false;
+            playerIsOverRequestEntryPoint = false;
+        }
+    }
+
+}
---
+++ Inside-Job/Assets/Scripts/GameObject/Player/PlayerControls.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2d;
+
+    public float PlayerSpeed;
+
+    public GameObject requestHolder;
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        this.MovePlayer();
+    }
+
+
+    private void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 position = rigidbody2d.position;
+
+        position += move * PlayerSpeed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
